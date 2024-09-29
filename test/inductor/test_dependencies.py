21581e9fcecb084fc54e3735fbe41cec63d66c50
Tom Ritchford <tom@swirly.com>
2024-09-29 18:51:40 +0000

    Remove unused Python variables in test/[e-z]*
    ghstack-source-id: 64f372bb3e2b064e057a40b3da69d6a5ed6c4137
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/136964

--- test/inductor/test_dependencies.py
+++ test/inductor/test_dependencies.py
@@ -117,10 +117,6 @@ class TestDependencies(InductorTestCase):
     def test_normalize_with_stride_order_equal(self):
         x = sympy_index_symbol("x")
         y = sympy_index_symbol("y")
-        var_ranges = {
-            x: 1024,
-            y: 2048,
-        }
 
         loop_order1 = MemoryDep(
             "access_the_same_buffer",
@@ -142,10 +138,6 @@ class TestDependencies(InductorTestCase):
     def test_normalize_with_stride_order_unequal(self):
         x = sympy_index_symbol("x")
         y = sympy_index_symbol("y")
-        var_ranges = {
-            x: 1024,
-            y: 2048,
-        }
 
         loop_order1 = MemoryDep(
             "access_the_same_buffer",
